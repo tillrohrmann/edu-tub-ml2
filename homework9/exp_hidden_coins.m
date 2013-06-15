957895e4d9b6816e5cdc624d1d2b6c0bec9312ec
Kay Fleischmann <fleischmann.kay@googlemail.com>
2013-06-16 01:34:55 +0200

    implementation final

--- homework9/exp_hidden_coins.m
+++ homework9/exp_hidden_coins.m
@@ -1,5 +1,19 @@
+% Experiment Hidden Markov Model
+%
+% Author:   ROHRMANN, TILL
+%           FLEISCHMANN, KAY
 
 
-[states, prob] = hidden_coins(2)
-[states, prob] = hidden_coins(20)
-
+% Generate 1000 times a random state-sequence and observations
+N=1000;
+len=20;
+X=zeros(N,1);
+tails=0;
+heads=0;
+for i=1:N
+    [states, output] = hidden_coins(len);
+    tails=tails+sum(output==2)/len;
+    heads=heads+sum(output==1)/len;
+    X(i)=heads/tails;
+end
+plot(X);
--- homework9/hidden_coins.m
+++ homework9/hidden_coins.m
@@ -1,29 +1,21 @@
-function [states,prob] = hidden_coins( n )
+function [states,output] = hidden_coins( n )
 %
 % Author:   ROHRMANN, TILL
 %           FLEISCHMANN, KAY
 
-
 trans = [.1 0.9;0.5 0.5];
 emis = [0.2 0.8; 0.4 0.6];
-initial = [1 0];
-states = [1 1; 1 2; 2 1; 2 2;];
-states = [1 1 1; 1 1 2; 1 2 1;1 2 2;2 1 1; 2 2 1; 2 2 2; 2 1 2;];
-N=length(states);
-len=size(states,2);
-seq = ones(N,len)*2;
 
-% compute p(x) sequence-transition probabilties
-px_seq = horzcat( initial( states(:,1))', trans( sub2ind( size(trans), states(:,1:end-1),  states(:,2:end)) ));
-px = prod( px_seq,2);
+states = zeros(n,1);
+output = zeros(n,1);
 
-%compute p(y|x)
-pyx = prod( px_seq .* emis( sub2ind( size(emis), seq, states )), 2);
+% fist default state
+states(1)=1;
+output(1)=(rand > emis(1,1))+1;
 
-py = sum(pyx.*px);
-
-pxy = (pyx.*px)./py;
-
-prob=pxy;
-
+for i=2:n
+    x = trans(states(i-1),:);
+    next_state=(rand > x(1));
+    states(i)=(next_state+1);
+    output(i)=(rand > emis(states(i),1))+1;
 end
