192bffc27ba79d6d82cb2150471b112174a34402
Kay Fleischmann <fleischmann.kay@googlemail.com>
2013-06-18 17:57:27 +0200

    viterbi matlab

--- homework10/experiment.m
+++ homework10/experiment.m
@@ -1,1 +1,9 @@
 % Experiment
+
+A = [0.1 0.9;0.5 0.5;];
+B = [0.2 0.8;0.4 0.6;];
+PI = [1 0];
+% observation
+y = [1 1 2 2 2 1 1 ];
+
+x = viterbi( A,B, PI, y )
--- homework10/viterbi.m
+++ homework10/viterbi.m
@@ -8,5 +8,35 @@ function x = viterbi(A, B, pi, y)
 % 
 % Author: KAY FLEISCHMANN,TILL ROHRMANN
 %
+numX=length(A); % num states
+numY=size(B,2); % num possible observations
+lenT=length(y); % time
+x = zeros(1,lenT); % output
+
+% init
+T1 = zeros(numX, lenT ); %store max probability from t-1
+T2 = zeros(numX, lenT ); % track best path
+t=1; % process first time step
+T1(:,t) = pi' .* B( :, y(t));
+
+for t=2:lenT
+    [val,index] = max ( (repmat( T1(:,t-1)', numX, 1 )  .* A) , [], 2) ;
+    T1(:,t) = val  .* B( :, y(t) );
+    T2(:,t) = index;
+end
+%T1
+%T2
+[v,z]=max( T1(:,lenT) );
+x(lenT)=T2(z,lenT);
+
+% find the best path
+for t=lenT:-1:2
+    [v,z]=max( T2(z,t) );
+    
+    x(t-1)=T2(z,t);
+end
+
+%T1
+%T2
 
 end
