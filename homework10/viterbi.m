2b57e5ac8565514d3f810115ee56f562b7a9c0bd
Kay Fleischmann <fleischmann.kay@googlemail.com>
2013-06-18 09:36:47 +0200

    mask

--- homework10/viterbi.m
+++ homework10/viterbi.m
@@ -1,9 +1,12 @@
 function x = viterbi(A, B, pi, y)
-%
-%
-%
+% input     A   tansitions between states
+%           B   emission probabilties
+%               from each state
+%           pi  initial state probabilities
+%           y   observed sequences
+% output    most likely sequences
+% 
 % Author: KAY FLEISCHMANN,TILL ROHRMANN
 %
-
 
 end
