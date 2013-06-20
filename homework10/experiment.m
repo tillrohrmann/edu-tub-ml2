80733daba4b6673eaa7e0c9fa3da653dbdf8be7e
Kay Fleischmann <fleischmann.kay@googlemail.com>
2013-06-20 14:06:04 +0200

    viterbi v1

--- homework10/experiment.m
+++ homework10/experiment.m
@@ -1,9 +1,17 @@
 % Experiment
 
+
+
 A = [0.1 0.9;0.5 0.5;];
 B = [0.2 0.8;0.4 0.6;];
 PI = [1 0];
 % observation
-y = [1 1 2 2 2 1 1 ];
+y = [1 1 2 2 2 1 1 1 1 1 1 1 1 2 1 ];
 
-x = viterbi( A,B, PI, y )
+x1 = viterbi( A,B, PI, y );
+
+
+%seq1 = hmmtrain(100,A,B);
+x2 = hmmviterbi(y,A,B);
+x1
+x2
