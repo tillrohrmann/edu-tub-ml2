d10c466699fc5811caa190478168b5dcbc3f3e60
Kay Fleischmann <fleischmann.kay@googlemail.com>
2013-06-21 11:12:21 +0200

    final code

--- homework10/viterbi.m
+++ homework10/viterbi.m
@@ -12,6 +12,7 @@ numX=length(A); % num states
 numY=size(B,2); % num possible observations
 lenT=length(y); % time
 x = zeros(1,lenT); % output
+Z = zeros(1,lenT); % output
 
 % init
 T1 = zeros(numX, lenT ); %store max probability from t-1
@@ -20,20 +21,23 @@ t=1; % process first time step
 T1(:,t) = pi' .* B( :, y(t));
 
 for t=2:lenT
-    [val,index] = max ( (repmat( T1(:,t-1)', numX, 1 )  .* A) , [], 2) ;
-    T1(:,t) = val  .* B( :, y(t) );
-    T2(:,t) = index;
+    for j=1:numX
+        [v,k] = max(T1(:,t-1) .* A(:,j) .* B(j,y(t)) );
+        T1(j,t) =v;
+        T2(j,t) =k;
+    end
+    
 end
-%T1
-%T2
-[v,z]=max( T1(:,lenT) );
-x(lenT)=T2(z,lenT);
+
+
+[v,k]=max( T1(:,lenT) );
+Z(lenT)=k;
+x(lenT)=Z(lenT);
 
 % find the best path
 for t=lenT:-1:2
-    [v,z]=max( T2(z,t) );
-    
-    x(t-1)=T2(z,t);
+    Z(t-1)=T2(Z(t),t);
+    x(t-1)=Z(t-1);
 end
 
 %T1
