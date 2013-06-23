29150b280712c3be603cc6641d331b1ce349f417
Till Rohrmann <till@nb.(none)>
2013-06-23 23:29:42 +0200

    minor changes

--- homework10/experiment.m
+++ homework10/experiment.m
@@ -1,6 +1,12 @@
 % Experiment
+%
+% Authors:  Kay Fleischmann
+%           Till Rohrmann
+close all;
+clear all;
+clc;
 A = [0.1 0.9;0.5 0.5;];
-B = [0.2 0.8;0.4 0.6;];
+B = [0.2 0.8;0.4 0.6];
 PI = [1 0];
 
 %test viterbi
@@ -21,35 +27,46 @@ for i=1:length(l)
     random_states(random_states>=0.5)=1;
     random_states(random_states<0.5)=2;
     
+    stationary_distribution = [0.3571, 0.6429];
+    randomNumbers = rand(N,l(i));
+    stationary_states = zeros(N,l(i));
+    stationary_states(randomNumbers < stationary_distribution(1)) =1;
+    stationary_states(randomNumbers >= stationary_distribution(1)) = 2;
+    
     viterbi_results = zeros(N,l(i));
     random_results = zeros(N,l(i));
+    stationary_results = zeros(N,l(i));
     
     % generate sequences and prove results
     % viterbi
     for n=1:N
-        [states,output] = hidden_coins( l(i) );
+        [states,output] = hidden_coins( A,B, l(i) );
         seq = viterbi( A,B, PI, output );
 
         viterbi_results(n,:) = states == seq;
         random_results(n,:) = states == random_states(n,:);
+        stationary_results(n,:) = states == stationary_states(n,:);
         
     end
     
     corr_viterbi = sum(viterbi_results) / N;
     corr_random = sum(random_results) / N;
+    corr_stationary = sum(stationary_results) /N;
 
-    total_corr_viterbi = sum( sum(viterbi_results)) / (N*l(i));
-    total_random_viterbi = sum( sum(random_results)) / (N*l(i));
+    total_corr_viterbi = sum( sum(viterbi_results,2)==l(i)) / (N);
+    total_random_viterbi = sum( sum(random_results,2)==l(i)) / (N);
+    total_stationary_viterbi = sum(sum(stationary_results,2)==l(i))/N;
     
     subplot(length(l),1,i); 
     hold on;
     plot(corr_viterbi)
     
-    title(['sequence length l= ' int2str( l(i) ) ', total viterbi: ' num2str(total_corr_viterbi) ', total random: ' num2str(total_random_viterbi)])
+    title(['sequence length l= ' int2str( l(i) ) ', whole sequence viterbi: ' num2str(total_corr_viterbi) ', whole sequence random: ' num2str(total_random_viterbi) ', whole sequence stationary: ' num2str(total_stationary_viterbi)]);
     
 
     plot(corr_random,'red');
-    legend('Viterbi','Random')
+    plot(corr_stationary,'black');
+    legend('Viterbi','Random', 'Stationary')
 
     hold off;
     
